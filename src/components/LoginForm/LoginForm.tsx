3a3279b22799234f7eff489f927d2613ef2bec72
HSushynska <stoyanova.gv@gmail.com>
2025-04-13 13:17:04 +0200

    add login form

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,11 +1,11 @@
-import { Error, useFormik } from "formik";
+import {useFormik } from "formik";
 import * as Yup from "yup";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Button from "../Button/Button";
 import Input from "../Input/Input";
-import { LoginFormComponent, Title } from "./styles";
+import { LoginFormComponent, Title, Error } from "./styles";
 import { LoginFormValues } from "./types";
 import Spinner from "../Spinner/Spinner";
 import { UserContext } from "../Layout/Layout";
@@ -29,7 +29,6 @@ function LoginForm() {
       .max(40, "Max 40 symbols")
       .min(12, "Min 12 symbols"),
     password: Yup.string()
-      .typeError("Password must be a number")
       .required("Field password is requered")
       .matches(
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
